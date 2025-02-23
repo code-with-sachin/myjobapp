200db0d993c0e9852de6e66e8861559261a88dbe
Sachin <sachinkaushikofficial@gmail.com>
2025-02-23 12:24:12 +0530

    delete job logic

--- src/main/java/com/sachinsk/myjobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/sachinsk/myjobapp/job/impl/JobServiceImpl.java
@@ -5,6 +5,7 @@ import com.sachinsk.myjobapp.job.JobService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -27,11 +28,24 @@ public class JobServiceImpl implements JobService {
 
     @Override
     public Job getJobById(Long id) {
-        for(Job job : jobs){
-            if (job.getId().equals(id)){
+        for (Job job : jobs) {
+            if (job.getId().equals(id)) {
                 return job;
             }
         }
         return null;
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        Iterator<Job> iterator = jobs.iterator();
+        while (iterator.hasNext()) {
+            Job job = iterator.next();
+            if (job.getId().equals(id)) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
     }
 }
